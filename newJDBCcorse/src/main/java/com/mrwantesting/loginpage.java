3c776891f03ad00c0f158144912f4dd1c3ed3749
Marwan Jaballah <115611296+maroiane2@users.noreply.github.com>
2024-04-17 16:40:13 +0100

    Add files via upload

---
+++ newJDBCcorse/src/main/java/com/mrwantesting/loginpage.java
@@ -0,0 +1,14 @@
+package com.mrwantesting;
+
+public class loginpage {
+
+
+
+    public static void main(String[] args){
+        MyFirstJFrame log = new MyFirstJFrame();
+        log.show();
+    }
+}
+
+
+
